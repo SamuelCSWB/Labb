7811ab34823d67a21f67800473c7b9ac07d17375
Samuel W Berkelund <samuel.berkelund@gmail.com>
2024-10-21 00:29:56 +0200

    klar med formuläret, och börjat med datalagret

---
+++ Labb/User.cs
@@ -0,0 +1,54 @@
+﻿namespace Labb
+{
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public Company Company { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(int id, string name, string email, Address address, Company company)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Address = address;
+            Company = company;
+        }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+
+        public Address(string street, string city, int zip)
+        {
+            Street = street;
+            City = city;
+            Zip = zip;
+        }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public string CatchPhrase { get; set; }
+
+        public Company(string name, string catchPhrase)
+        {
+            Name = name;
+            CatchPhrase = catchPhrase;
+        }
+    }
+    
+
+}
+
---
+++ Labb/UserDataAccess.cs
@@ -0,0 +1,21 @@
+﻿namespace Labb
+{
+    public class UserDataAccess
+    {
+        private List<User> _users = new List<User>()
+        {
+            new User(1, "Ken", "ken@bio.io", new Address("Aldervägen 1", "Stockholm", 123), new Company("Postnord", "We make everyday life easier")),
+            new User(2, "Ben", "ben@bio.io", new Address("Björkvägen 2", "Stockholm", 123), new Company("Postnord", "We make everyday life easier")),
+            new User(3, "Jen", "jen@bio.io", new Address("Cederstigen 3", "Stockholm", 123), new Company("Postnord", "We make everyday life easier")),
+            new User(4, "Len", "len@bio.io", new Address("Dovägen 4", "Stockholm", 123), new Company("Postnord", "We make everyday life easier")),
+            new User(5, "Ren", "ren@bio.io", new Address("Ekskogen 5", "Stockholm", 123), new Company("Postnord", "We make everyday life easier")),
+
+        };
+
+        public List<User> GetUsers()
+        {
+            return _users;
+        }
+
+    }
+}
