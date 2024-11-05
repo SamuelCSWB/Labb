309b7ba772298a2078ff7c9efd311cf047f08ecf
Samuel W Berkelund <samuel.berkelund@gmail.com>
2024-11-05 19:36:46 +0100

    Alla funktioner har jag gjort till egna metoder. och uppdaterat mitt datalagers användare.

--- Labb/UserDataAccess.cs
+++ Labb/UserDataAccess.cs
@@ -4,18 +4,20 @@
     {
         private readonly IEnumerable<User> _users = new List<User>()
         {
-            new User(1, "Ken", "ken@bio.io", new Address("Aldervägen 1", "Stockholm", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(2, "Ben", "ben@bio.io", new Address("Björkvägen 2", "Stockholm", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(3, "Jen", "jen@bio.io", new Address("Cederstigen 3", "Stockholm", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(4, "Len", "len@bio.io", new Address("Dovägen 4", "Stockholm", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(5, "Ren", "ren@bio.io", new Address("Ekskogen 5", "Stockholm", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(6, "Fen", "fen@bio.io", new Address("Askervägen 10", "Malmö", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(7, "Gen", "gen@bio.io", new Address("Björkvägen 20", "Malmö", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(8, "Hen", "hen@bio.io", new Address("Cederstigen 30", "Malmö", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(9, "Ien", "ien@bio.io", new Address("Dovägen 40", "Malmö", "123"), new Company("Postnord", "We make everyday life easier")),
-            new User(10, "Pen", "pen@bio.io", new Address("Dovägen 230", "Malmö", "123"), new Company("Postnord", "We make everyday life easier"))
-
+            new User(1, "Magic Johnson", "magic.johnson@nba.com", new Address("Aldervägen 1", "Gävle", "803 20"), new Company("IKEA", "Creating a better everyday life")),
+            new User(2, "Kevin Durant", "kevin.durant@nba.com", new Address("Björkvägen 2", "Stockholm", "114 25"), new Company("Spotify", "Music for everyone")),
+            new User(3, "Ben Simmons", "ben.simmons@nba.com", new Address("Cederstigen 3", "Östersund", "831 45"), new Company("Scania", "Driving the shift")),
+            new User(4, "Lonzo Ball", "lonzo.ball@nba.com", new Address("Dovägen 4", "Borås", "503 37"), new Company("Ericsson", "Empowering an intelligent, sustainable, and connected world")),
+            new User(5, "Jimmy Butler", "jimmy.butler@nba.com", new Address("Ekskogen 5", "Göteborg", "411 27"), new Company("Volvo", "For life")),
+            new User(6, "Pelle Larsson", "pelle.larsoon@nba.com", new Address("Askervägen 10", "Malmö", "211 37"), new Company("Skanska", "Building for a better society")),
+            new User(7, "Goran Dragic", "goran.dragic@nba.com", new Address("Björkvägen 20", "Sundsvall", "852 34"), new Company("Telia", "Connecting people")),
+            new User(8, "Lebron James", "lebron.james@nba.com", new Address("Cederstigen 30", "Nässjö", "571 32"), new Company("H&M", "Fashion and quality at the best price")),
+            new User(9, "Allen Iverson", "allen.iverson@nba.com", new Address("Dovägen 40", "Umeå", "903 25"), new Company("Electrolux", "Shape living for the better")),
+            new User(10, "Luka Doncic", "luka.doncic@nba.com", new Address("Dovägen 230", "Luleå", "972 41"), new Company("Sandvik", "Advancing the world through engineering"))
         };
+
+
+
 
 
         public async Task<IEnumerable<User>> GetUsers()
